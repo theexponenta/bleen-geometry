d8e804e204af0c083b545bf639269323314c8c93
Exponenta <the.exponenta@gmail.com>
2024-12-14 11:33:29 +0300

    Add dependency to Point object from Construct object

--- Objects/Point/Point.d
+++ Objects/Point/Point.d
@@ -2,6 +2,6 @@
 Point.BorderPen dd ?
 Point.SelectedBorderPen dd ?
 
-Point.DependencyObjectsIdsOffsets dd 1, Point.IntersectionId
+Point.DependencyObjectsIdsOffsets dd 2, Point.IntersectionId, Point.ConstructObject1Id
 
 Point.StrFormat du "Point %s", 0
